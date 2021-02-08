0fb3db46b38f8aecf09383483f55ada75cf45b08
L3h0n4rd0 <leonardoferreira@protonmail.com>
2021-02-08 19:57:25 -0300

    exercicios

--- des/d004/d004.py
+++ des/d004/d004.py
@@ -1,3 +1,4 @@
 primeiro = int(input('Insira o primeiro número '))
 segundo = int(input('Insira o segundo número '))
-print(primeiro+segundo)
+soma = int(primeiro+segundo)
+print('A soma entre {} e {} vale {}'.format(primeiro,segundo,soma))
---
+++ des/d005/d006.py
@@ -0,0 +1,2 @@
+n1 = bool(input('digite um valor '))
+print(type(n1))
