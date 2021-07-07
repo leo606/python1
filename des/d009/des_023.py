afe283976c4256d5d598b7a31bfb83da1fc64537
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-07 18:26:45 -0300

    d023 str index

---
+++ des/d009/des_023.py
@@ -0,0 +1,8 @@
+# ler um numero de 0 a 9999 e mostrar cada um dos digitos separados
+
+num = str(input('entre com um numero de 0 a 9999 '))
+
+print('unidade ', num[3])
+print('dezena ', num[2])
+print('centena ', num[1])
+print('milhar ', num[0])
